03cac5a2fe1d1364df3d02bff2edfef5b1492965
Selahaddin Turkoglu <90051755+turkogl3@users.noreply.github.com>
2023-02-10 01:54:57 -0600

    Created a Main menu UI and implemented a Quit function in MainMenu script.

---
+++ Assets/Scripts/Mechanics/MainMenu.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+ //   public void ComputerScience(){
+          //  SceneManager.LoadScene(SceneManager.GetActiveScene().Buildindex +1);
+  //  }
+    public void Quit(){
+
+        Application.Quit();
+        Debug.Log("Player has quit the game");
+    }
+}
